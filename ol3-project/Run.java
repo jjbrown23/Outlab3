5472d627680282788bac1c64d0b5ebf43d62b961
jjbrown23 <71121776+jjbrown23@users.noreply.github.com>
2020-11-02 17:59:52 -0700

    Add files via upload

---
+++ ol3-project/Run.java
@@ -0,0 +1,21 @@
+/* *****************************************************************************
+ *  Name:    Ada Lovelace
+ *  NetID:   alovelace
+ *  Precept: P00
+ *
+ *  Description:  Prints 'Hello, World' to the terminal window.
+ *                By tradition, this is everyone's first program.
+ *                Prof. Brian Kernighan initiated this tradition in 1974.
+ *
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Run {
+    public static void main(String[] args) {
+
+        StdOut.println("Hello World");
+        StdOut.println("I guess it works now");
+
+    }
+}
